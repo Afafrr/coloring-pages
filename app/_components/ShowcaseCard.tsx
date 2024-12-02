a50c44633b53c81865eafb1d5d39b0d60e96b9b4
Afafrr <mateusz.aftanski@gmail.com>
2024-12-02 17:08:44 +0100

    FEAT: showcase in main page

---
+++ app/_components/ShowcaseCard.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Image from "next/image";
+import { Box } from "@mui/material";
+import { Card } from "@mui/material";
+import { StaticImageData } from "next/image";
+type cardProps = {
+  image1: StaticImageData;
+  image2: StaticImageData;
+  alt1: string;
+  alt2: string;
+};
+function ShowcaseCard({ images }: { images: cardProps }) {
+  const { image1, image2, alt1, alt2 } = images;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        transform: "translateY(-10px)",
+      }}
+    >
+      <Card
+        sx={{
+          height: "auto",
+          aspectRatio: "3/4",
+          width: { xs: "150px", md: "175px" },
+          position: "relative",
+          transform: "translateX(20px) rotate(-8deg)",
+          zIndex: 10,
+          transition: "scale 0.5s ease-in-out, box-shadow 0.1s ease-in-out",
+          "&:hover": {
+            boxShadow: 3,
+            transform: "scale(1.01) translateX(20px) rotate(-8deg)",
+          },
+        }}
+      >
+        <Image
+          fill
+          src={image1}
+          alt={alt1}
+          style={{
+            objectFit: "cover",
+          }}
+          quality={1}
+        />
+      </Card>
+      <Card
+        sx={{
+          position: "relative",
+          transform: "translateX(-20px) rotate(10deg)",
+          height: "auto",
+          width: { xs: "150px", md: "175px" },
+          transition: "scale 0.5s ease-in-out, box-shadow 0.1s ease-in-out",
+          "&:hover": {
+            boxShadow: 3,
+            transform: "scale(1.01) translateX(-20px) rotate(10deg)",
+          },
+        }}
+      >
+        <Image
+          fill
+          src={image2}
+          alt={alt2}
+          style={{
+            objectFit: "cover",
+          }}
+          quality={10}
+        />
+      </Card>
+    </Box>
+  );
+}
+
+export default ShowcaseCard;
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import mountainSvg from "@/public/Home/mountainSvg.svg";
 import styles from "./styles/home.module.css";
 import { Shadows_Into_Light_Two } from "next/font/google";
 import NavButton from "./_components/NavButton";
+import ShowcaseCard from "./_components/ShowcaseCard";
+import { showcaseCardsData } from "./assets/showcaseCardData";
 
 const comic = Shadows_Into_Light_Two({
   subsets: ["latin"],
@@ -13,63 +15,133 @@ const comic = Shadows_Into_Light_Two({
 
 export default function Home() {
   return (
-    <div>
-      <Container
+    <Box>
+      <Box
+        component="section"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          height: "100vh",
+          position: "relative",
+          maxHeight: "100vh",
+          height: "100%",
         }}
       >
-        <Image src={sunSvg} alt="Rysunek słońca" className={styles.sunImage} />
-        <Box
+        <Container
           sx={{
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            textAlign: "center",
-            mb: "50px",
+            width: "100%",
+            height: "100vh",
           }}
         >
-          <Typography
-            variant="h1"
-            sx={{
-              display: "inline-block",
-            }}
-          >
-            Kolorowanki inspirowane twoją
-          </Typography>
-          <Typography
-            className={`${comic.className} ${styles.rainbowText}`}
-            variant="h1"
-            sx={{
-              display: "inline-block",
-              p: "5px",
-              textTransform: "uppercase",
-            }}
-          >
-            wyobraźnią
-          </Typography>
-          <Typography
-            sx={{ maxWidth: "600px", mt: "20px", fontWeight: "200" }}
-            variant="h4"
-          >
-            Wpisz postać, scenę czy krajobraz, a sztuczna inteligencja
-            wygeneruje dla ciebie obrazy
-          </Typography>
-          <NavButton />
           <Image
-            src={mountainSvg}
-            alt="Rysunek gór"
-            className={styles.mountainImage}
+            src={sunSvg}
+            alt="Rysunek słońca"
+            className={styles.sunImage}
           />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "center",
+              mb: "50px",
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                display: "inline-block",
+              }}
+            >
+              Kolorowanki inspirowane twoją
+            </Typography>
+            <Typography
+              className={`${comic.className} ${styles.rainbowText}`}
+              variant="h1"
+              sx={{
+                display: "inline-block",
+                p: "5px",
+                textTransform: "uppercase",
+              }}
+            >
+              wyobraźnią
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ maxWidth: "600px", mt: "20px", fontWeight: "200" }}
+            >
+              Wpisz postać, scenę czy krajobraz, a sztuczna inteligencja
+              wygeneruje dla ciebie obrazy
+            </Typography>
+            <NavButton />
+            <Image
+              src={mountainSvg}
+              alt="Rysunek gór"
+              className={styles.mountainImage}
+            />
+          </Box>
+        </Container>
+      </Box>
+      <Container
+        component="section"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+        }}
+      >
+        <Typography variant="h2" fontWeight={500} sx={{ mt: "50px" }}>
+          Kilka kliknięć, a wygenerujesz co chcesz.
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "50px",
+            mt: "40px",
+            overflow: "hidden",
+          }}
+        >
+          {showcaseCardsData.map((card, index) => {
+            const isEven = index % 2 === 0;
+            return (
+              <Box
+                key={index}
+                sx={{
+                  display: "flex",
+                  flexDirection: {
+                    xs: "column",
+                    sm: `${isEven ? "row" : "row-reverse"}`,
+                  },
+                  justifyContent: `${isEven ? "start" : "end"}`,
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+              >
+                <ShowcaseCard images={card} />
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: `${isEven ? "start" : "end"}`,
+                    textAlign: `${isEven ? "left" : "right"}`,
+                    maxWidth: "500px",
+                  }}
+                >
+                  <Typography variant="h4" color="primary">
+                    {card.title}
+                  </Typography>
+                  <Typography variant="body2">{card.description}</Typography>
+                </Box>
+              </Box>
+            );
+          })}
         </Box>
       </Container>
       <footer></footer>
-    </div>
+    </Box>
   );
 }
